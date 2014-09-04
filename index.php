99c00418d693db6ef8d0e9eb1c69c1a93bf5be3d
J. Ryan Kelley <ryan.kelley@espn.com>
2014-09-03 22:00:59 -0700

    Basic detection on whether we should show html or png

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
+<?php
+	if(isset($_GET["mode"]) && $_GET['mode'] == 'png') {
+		//header('Content-Type: image/png');
+		echo '<!-- We should just render the png in this case instead of all the other crap -->';
+	} else {
+		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+	}
+?>
 <html>
   <head>
     <title>HTML5 Logo Builder</title>
