65e39670b822a71e82c41f9c65f588dc6cb3971b
J. Ryan Kelley <ryan.kelley@espn.com>
2014-09-08 22:41:38 -0700

    rendering just a png src will be cool, but not right meow

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-	if(isset($_GET["mode"]) && $_GET['mode'] == 'png') {
-		//header('Content-Type: image/png');
-		echo '<!-- We should just render the png in this case instead of all the other crap -->';
-	} else {
-		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+	$mode = "generic";
+	if(isset($_GET["mode"])) {
+		$mode = $_get["mode"]);
 	}
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
   <head>
     <title>HTML5 Logo Builder</title>
