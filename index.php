ba09c15d2f58ab4cdce1c7265830ccf43ebadf83
J. Ryan Kelley <ryan.kelley@espn.com>
2014-09-08 22:49:37 -0700

    get GET, and add styles

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	$mode = "generic";
 	if(isset($_GET["mode"])) {
-		$mode = $_get["mode"]);
+		$mode = $_GET["mode"];
 	}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -10,7 +10,7 @@
     <title>HTML5 Logo Builder</title>
 	<script src="js/jquery.js"> </script> 
     <script src="js/kinetic.js"> </script>
-
+    <link rel="stylesheet" charset="utf-8" href="css/styles.css" />
   </head>
   <body style="margin:auto;">
     <div id="logo"></div>
